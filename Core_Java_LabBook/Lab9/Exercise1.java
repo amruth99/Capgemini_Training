057604367cd2e127586651fd8e4f6dfbcc4b22ce
Amruth N <64019479+amruth99@users.noreply.github.com>
2021-03-22 15:42:24 +0530

    Update Exercise1.java

--- Core_Java_LabBook/Lab9/Exercise1.java
+++ Core_Java_LabBook/Lab9/Exercise1.java
@@ -7,9 +7,9 @@ import java.util.function.Predicate;
 public class Exercise1 {
 	public static void main(String[] args) {
 		Scanner read = new Scanner(System.in);
-		//System.out.println("Enter two integers:");
+		System.out.println("Enter two integers:");
 		BiFunction<Integer, Integer, Double> biF = (a, b) -> Math.pow(a, b);
-		//System.out.println(biF.apply(read.nextInt(), read.nextInt()));
+		System.out.println(biF.apply(read.nextInt(), read.nextInt()));
 		
 		BiFunction<Integer, Integer, Double> power = (x, y) -> Math.pow(x, y);
 		System.out.println(power.apply(2, 4));
